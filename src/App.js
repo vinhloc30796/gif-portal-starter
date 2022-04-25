6df5c7ee9e1fc3ccaa80b9c7a73d45fbc6836e83
Loc Nguyen <vinhloc30796@gmail.com>
2022-04-25 10:45:55 +0700

    Add checkIfWalletIsConnected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import React, { useEffect } from 'react'; /* TODO: what does useEffect do? */
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'vl307';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const checkIfWalletIsConnected = async () => {
+    /**
+     * Checks if a Phanton Wallet is connected to the browser.
+     */
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+        }
+      }
+      else {
+        alert ('Solana object not found! Get a Phantom Wallet at https://phantom.app/');
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * Runs the checkIfWalletIsConnected function when the component mounts.
+   */
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+
   return (
     <div className="App">
       <div className="container">
